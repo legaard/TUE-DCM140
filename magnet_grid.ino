3728ab789ddc9dd0efbac9a0463734de95b8543a
Lasse Legaard <legaard.lasse@gmail.com>
2016-11-28 17:13:39 +0100

    Added support for coordinates

--- magnet_grid.ino
+++ magnet_grid.ino
@@ -1,4 +1,5 @@
 #include <Stepper.h>
+#include <QueueArray.h>
 
 #define STEPS 32
 #define SPEED_FAST 640
@@ -19,25 +20,46 @@ int rightStepsRemaining = 0;
 
 int currentSpeed = SPEED_FAST;
 
-boolean firstTime = true;
+boolean isReadyForCommand = true;
+
+QueueArray <int> xCoordinates;
+QueueArray <int> yCoordinates;
 
 void setup() {
+  // setup serial (for debugging)
   Serial.begin(9600);
+  xCoordinates.setPrinter(Serial);
+  yCoordinates.setPrinter(Serial);
 
-  //Setup pins from 4-11
+  // setup pins from 4-11
   for (int i = 4; i <= 11; i++) {
     pinMode(i, OUTPUT);
   }
 
+  // adding coordinates to the arrays
+  xCoordinates.push(90); yCoordinates.push(180);
+  xCoordinates.push(180); yCoordinates.push(360);
+  xCoordinates.push(360); yCoordinates.push(180);
+
+  // setting the speed for the steppers
   setStepperSpeed(currentSpeed);
   Serial.println("Setup is done...");
 }
 
 void loop() {
-  if (firstTime) {
-    turn(LEFT, 360);
-    turn(RIGHT, 360);
-    firstTime = false;
+  //making sure that the number of x- and y-coordinates are the same
+  if (xCoordinates.count() != yCoordinates.count()) {
+    Serial.println("Uneven number of coordinates – please take care of it!");
+    delay(5000);
+    return;
+  }
+
+  //
+  if (isReadyForCommand && !xCoordinates.isEmpty() && !yCoordinates.isEmpty()) {
+    int x = xCoordinates.dequeue();
+    int y = yCoordinates.dequeue();
+    turn(LEFT, x);
+    turn(RIGHT, y);
   }
 
   // needs to be call in every loop – can NOT be omitted
@@ -46,6 +68,8 @@ void loop() {
 
 /* HELPER METHODS */
 void updateStepperPositions() {
+  isReadyForCommand = (leftStepsRemaining <= 0 && rightStepsRemaining <= 0);
+
   if (leftStepsRemaining > 0) {
     leftStepper.step(leftStepperDirection ? 1 : -1);
     leftStepsRemaining = leftStepsRemaining - 1;
